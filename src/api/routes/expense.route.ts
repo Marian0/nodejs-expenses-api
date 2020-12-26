aded22b29e94d20f8f1c6e238b7d094733a13515
marian0 <marianosantafe@gmail.com>
2020-12-26 18:56:27 -0300

    Expenses model, service, routes and create endpoint

---
+++ src/api/routes/expense.route.ts
@@ -0,0 +1,43 @@
+import { celebrate } from 'celebrate';
+import { Request, Response, Router } from 'express';
+import Joi from 'joi';
+import Container from 'typedi';
+import { Expense } from '../../models/expense.model';
+import { User } from '../../models/user.model';
+import ExpenseService from '../../services/expense.service';
+import middlewares from '../middlewares';
+
+const route = Router();
+
+export default (app: Router) => {
+  app.use('/expenses', route);
+
+  /**
+   * POST api/expenses
+   * Creates an expense related to current logged user
+   */
+  route.post(
+    '/',
+    middlewares.isAuth,
+    celebrate({
+      body: Joi.object({
+        detail: Joi.string()
+          .min(2)
+          .max(30)
+          .required(),
+        date: Joi.date()
+          .required(),
+        amount: Joi.number()
+          .required(),
+      }),
+    }, {
+      abortEarly: false
+    }),
+    middlewares.attachCurrentUser,
+    async (req: Request, res: Response) => {
+      const expenseService = Container.get(ExpenseService);
+      const response = await expenseService.createExpense(req.body as Expense, req.currentUser as User);
+      return res.json(response).status(201);
+    },
+  );
+};
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import auth from './auth.route';
 import users from './user.route';
+import expense from './expense.route';
 
 const router = Router();
 auth(router);
 users(router);
+expense(router);
 
 export default router;
